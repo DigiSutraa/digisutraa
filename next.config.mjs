12686d060c839f2a52fc692839b820ca6ae25ea8
sachinupadhyay268 <sachinupadhyay268@gmail.com>
2024-09-30 16:35:17 +0530

    site skeleton developed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack(config) {
+      // Add SVG loader
+      config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+      });
+  
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
